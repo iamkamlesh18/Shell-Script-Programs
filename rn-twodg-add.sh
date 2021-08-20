93555b468846dbcea4fb1e8df74680cd1e5d8473
Kamlesh Patil <65329245+imkp1811@users.noreply.github.com>
2021-08-20 11:42:52 +0530

    Update rn-twodg-add.sh

--- rn-twodg-add.sh
+++ rn-twodg-add.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-rn1=$((RANDOM%20))
-rn2=$((RANDOM%20))
-rn3=$((RANDOM%20))
-rn4=$((RANDOM%20))
-rn5=$((RANDOM%20))
+rn1=$((RANDOM%10))
+rn2=$((RANDOM%10))
+rn3=$((RANDOM%10))
+rn4=$((RANDOM%10))
+rn5=$((RANDOM%10))
 
-echo $rn1
-echo $rn2
-echo $rn3
-echo $rn4
-echo $rn5
+echo "First Number:  "$rn1
+echo "Second Number: "$rn2
+echo "Third Number:  "$rn3
+echo "Fourth Number: "$rn4
+echo "Fifth Number:  "$rn5
 
 sum=$(( $rn1+$rn2+$rn3+$rn4+$rn5 ))
 echo "Sum is: $sum"
