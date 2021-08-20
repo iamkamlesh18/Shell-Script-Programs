23ac8ece04e95a1452960e053937687a9c82346c
Kamlesh Patil <65329245+imkp1811@users.noreply.github.com>
2021-08-20 10:47:15 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,13 @@
 class hello{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
+    public static void main(String args[]){ 
+        while(true){
+            System.out.println("Hello Java"); 
+            try{
+                Thread.sleep(60000);
+            }catch(Exception ex){
+                System.out.println(ex);
+            }
+        }
+      
     }  
 }  
