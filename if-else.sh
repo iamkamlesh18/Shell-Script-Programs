f84188e3e844e527b48de45e49d9c35b58e5e674
Kamlesh Patil <patilkamlesh1811@gmail.com>
2021-08-20 17:31:05 +0530

    first commit

--- if-else.sh
+++ if-else.sh
@@ -1,22 +1,26 @@
-#!/bin/bash -x
+#!/bin/bash  -x
 
-rn1=$((1 + $RANDOM % 100))
-rn2=$((1 + $RANDOM % 100))
+rn1=$(( 1 + $RANDOM % 100 ))
+rn2=$(( 1 + $RANDOM % 100 ))
 
 echo "First Random Number  " $rn1
 echo "Second Random Number " $rn2
 
 
-if [$rn1 -le $rn2];
-
-then
-	echo "First Random Number is lesser than Second"
+if	[ $rn1 -lt $rn2 ];
+then	
+	echo "First Number is lesser than Second"
     	echo "Please Try Your Luck..!! To Generate Similer Number"
 
-elif[$rn1 -ge $rn2]
-	echo "A is greater than B"
+elif	[ $rn1 -gt $rn2 ];
+then
+	echo "First Number is greater than Second"
+	echo "Please Try Your Luck..!! To Generate Similer Number"
+
 else
- 	echo "First Random Number Equal to Second"
+ 	echo "First Number Equal to Second"
   	echo "You Won..!!"
 
 fi
+
+
