94cf3d5679b269333f21f5500982fbccd010220e
Kamlesh Patil <patilkamlesh1811@gmail.com>
2021-08-20 20:34:25 +0530

    first commit

--- unitcon.sh
+++ unitcon.sh
@@ -1,15 +1,26 @@
 #!bin/bash -x
-read -p "enter the no" n
-read ch
-case "$ch" in
+
+read -p "Enter the value : " num
+echo "The Value You Provided is: "$num | awk '{print $0}'
+echo
+echo "which Conversion do you want?"
+echo "To feet type ft "
+echo "To inches type in"
+
+read -p "Enter the Conversion type ft/in :" ch
+echo
+echo "You selected : "$ch
+case $ch in 
 
 "ft")
-ft=$(( $n / 12 ))
-echo $ft
+
+ft=$(($num / 12 ))
+echo "feet : " $ft | awk '{print $0}'
 ;;
 
-"inch")
-in=$(( $n * 12 ))
-echo $in
+"in")
+in=$(( $num * 12 )) | awk '{print $0}'
+echo "inch : " $in
+;;
 
 esac
